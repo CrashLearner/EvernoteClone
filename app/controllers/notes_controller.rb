a4ae6637c694fdc47c019779c93f3280c074641f
Yonathan Ayenew <yonayen@gmail.com>
2015-02-20 16:56:00 -0800

    Added association between User and Notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -9,11 +9,11 @@ class NotesController < ApplicationController
 	end
 
 	def new
-		@note = Note.new
+		@note = current_user.notes.build
 	end
 
 	def create
-		@note = Note.new(note_params)
+		@note = current_user.notes.build(note_params)
 
 		if @note.save
 			redirect_to @note
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150221004145) do
+ActiveRecord::Schema.define(version: 20150221004726) do
 
   create_table "notes", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
